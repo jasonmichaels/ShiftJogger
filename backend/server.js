e7aa697e25367ae45f4861a064c155cf9c2ef04d
Jason White <jasonmichaelwhite@gmail.com>
2018-12-08 14:56:07 -0500

    minor edits to setup

---
+++ backend/server.js
@@ -0,0 +1,42 @@
+/* imports */
+
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
+const users = require("./routes/api/users");
+const logs = require("./routes/api/logs");
+
+const app = express();
+
+// Body parser middleware -- used in users.js
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+
+// DB config
+const db = require("./config/keys").mongoURL;
+
+// connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+// passport middleware
+app.use(passport.initialize());
+
+// passport config (passport jwt strategy)
+require("./config/passport")(passport);
+
+// user routes
+app.use("/api/users", users);
+app.use("/api/logs", posts);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
