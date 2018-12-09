df8aa0eaf7dfbe7c3aff32720c4ce80b5ef8ccf1
Jason White <jasonmichaelwhite@gmail.com>
2018-12-08 22:04:57 -0500

    add login validation placeholder

---
+++ backend/validation/login.js
@@ -0,0 +1,32 @@
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
+
+module.exports = validateLoginInput = data => {
+  // errors object used to supply information to login form
+  // if fields don't meet basic requirements, messages displayed client-side
+  // from fields object (if applicable)
+  let errors = {};
+
+  // since Validator's `isEmpty` only tests strings,
+  // need to convert possibly an empty field to an empty string.
+  // so, if not there at all, convert to empty string
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+
+  Validator.isEmpty(data.email)
+    ? (errors.email = "Email field is required")
+    : !Validator.isEmail(data.email)
+    ? (errors.email = "Email is invalid")
+    : null;
+
+  Validator.isEmpty(data.password)
+    ? (errors.password = "Pasword field is required")
+    : !Validator.isLength(data.password, { min: 6, max: 30 })
+    ? (errors.password = "Pasword must be between 6 and 30 characters")
+    : null;
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
