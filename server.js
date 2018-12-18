bbedb3ead1351a3680ac33fb68817934d495868d
Jason White <jasonmichaelwhite@gmail.com>
2018-12-18 01:58:57 -0500

    modified server.js static path response v. prod/dev

--- server.js
+++ server.js
@@ -40,9 +40,14 @@ app.use("/api/users", users);
 if (process.env.NODE_ENV === "production") {
   // set static folder
   app.use(express.static("client/build"));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  app.get("/*", (req, res) =>
+    res.sendFile(path.join(__dirname, "./client/build/index.html"))
   );
+} else {
+  app.use(express.static(path.join(__dirname, "/client/public")));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+  });
 }
 
 const port = process.env.PORT || 5000;
