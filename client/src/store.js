c527278232b16687c49206d5d6c94496d55c2958
Jason White <jasonmichaelwhite@gmail.com>
2018-12-19 17:02:37 -0500

    fix for redux f is undefined

--- client/src/store.js
+++ client/src/store.js
@@ -11,6 +11,9 @@ export const store = createStore(
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+      : f => f
   )
 );
