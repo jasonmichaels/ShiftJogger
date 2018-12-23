f47fd790f8bcb4494add0a57f6f000ec2a337ccb
Jason White <jasonmichaelwhite@gmail.com>
2018-12-23 11:45:47 -0500

    more work on relative imports after file reorganization

--- client/src/components/styled-components/HeaderStyles.js
+++ client/src/components/styled-components/HeaderStyles.js
@@ -1,16 +1,16 @@
 import styled from "styled-components";
 
-// export const HeaderMainStyle = styled.header`
-//   display: flex;
-//   align-items: center;
-//   height: 2.5rem;
-//   text-transform: uppercase;
-//   font-size: 1em;
-// `;
+export const HeaderMainStyle = styled.header`
+  display: flex;
+  align-items: center;
+  height: 2.5rem;
+  text-transform: uppercase;
+  font-size: 1em;
+`;
 
-// export const AppTitle = styled.h1`
-//   margin: 0 auto 0 auto;
-// `;
+export const AppTitle = styled.h1`
+  margin: 0 auto 0 auto;
+`;
 
 export const HeaderTextStyle = styled.h2`
   margin: 2rem 0;
