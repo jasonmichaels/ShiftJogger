42461555318bc1a6c91647c8da1fd3242a7b19c9
Jason White <jasonmichaelwhite@gmail.com>
2019-01-13 04:40:10 -0500

    heroku tweaking v1

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use("/api/users", users);
 if (process.env.NODE_ENV === "production") {
   // set static folder
   app.use(express.static("client/build"));
-  app.get("/*", (req, res) =>
+  app.get("*", (req, res) =>
     res.sendFile(path.join(__dirname, "./client/build/index.html"))
   );
 } else {
